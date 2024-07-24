abefdf8f9e8af4b87a8140a8ce0d23bee469554a
DharsanB-sparkout <dharsan2226@gmail.com>
2024-07-24 17:03:15 +0530

    commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  dropdown(): void { 
+    const dropdownBtn = document.getElementById("dropdownbtn") as HTMLElement;
+    if (dropdownBtn) {
+        if (dropdownBtn.style.display === 'none' || dropdownBtn.style.display === '') {
+            dropdownBtn.style.display = 'block';
+        } else {
+            dropdownBtn.style.display = 'none';
+        }
+    }
 }
+  
+}
+
+
